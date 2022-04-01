009116b31cdca4bb696077071781854fc27b6a03
PreVizsla0410 <richardszigety@gmail.com>
2022-04-01 20:07:49 +0200

    trying

---
+++ components/hero.php
@@ -0,0 +1,5 @@
+<div class="bgimg bg-info"> 
+    <div class="d-flex justify-content-center ">
+      <h5 class="align-middle" >Ranked Nr.87 in alternative journeys!</h5>
+   </div>
+  </div>
---
+++ index.php
@@ -0,0 +1,69 @@
+<?php 
+require_once 'actions/db_connect.php';
+
+$sql = "SELECT * FROM locations";
+$result = mysqli_query($connect ,$sql);
+$tbody=''; 
+if(mysqli_num_rows($result)  > 0) {     
+    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){         
+        $tbody .=   ' 
+        <div class="col">
+        <div class="card-group h-200  ">
+          <img src="pictures/'.$row['image'].'" class="card-img-top" alt="'.$row['holidayname'].'">
+          <div class="card-body bg-transparent text-dark">
+            <h5 class="card-title">'.$row['holidayname'].'</h5>
+            <p class="card-text">'.$row['description'].'</p>
+            <p class="card-text">Duration: '.$row['length-of-holiday'].'</p>
+            <p class="card-text">Price: '.$row['price'].'€</p>
+            <div class="col text-center">
+            <a href="update.php?id=' .$row['id'].'"><button class="btn btn-primary" type="button" title="Edit the Product">Edit</button></a>
+            <a href="delete.php?id=' .$row['id'].'"><button class="btn btn-danger" type="button" title="Delete the Product">Delete</button></a>
+            <a href="moredetails.php?id=' .$row['id'].'"><button class="btn btn-success" type="button" title="Show more info">Interested!</button></a>
+                </div>
+            </div>
+        </div>
+      </div>
+            ';
+        
+    };
+} else {
+    $tbody =  "<tr><td colspan='5'><center>No Locations Available </center></td></tr>";
+}
+
+mysqli_close($connect);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Mount Everest Travel Agency</title>
+        <link rel="stylesheet" href="style/index.css">
+        <?php require_once 'components/boot.php'?>
+    </head>
+    <body>
+    <header>
+        <?php include_once 'components/navigation.php';   ?>
+        <?php include_once 'components/hero.php';   ?>
+
+    </header>
+       <main class="container container w-75 mt-3 mb-3" id="offers">
+           <div class="text-center m-5">
+           <h2> <i>Make an alternative journey this time!</i> </h2>
+           <hr>
+           </div>
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+            <tbody>
+                    <?= $tbody;?>
+                </tbody>
+                </div>
+                </div>
+                <hr>
+        </main>
+      
+        <footer>
+            <?php require_once 'components/footer.php' ?>
+        </footer>
+    </body>
+</html>
